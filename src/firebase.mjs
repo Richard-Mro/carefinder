4df591e5df4571d85322d97a7c308bdb672f9dbe
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-13 01:55:27 +0100

    Sixteenth Commit

--- src/firebase.mjs
+++ src/firebase.mjs
@@ -5,9 +5,9 @@ import { getAuth } from 'firebase/auth'
 
 // Define Firebase configuration using Vite environment variables
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
