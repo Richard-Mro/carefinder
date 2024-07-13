8283802e7598eabe9675a20d29dd269fd8ea877e
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-13 19:15:03 +0100

    Another Commit

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-
+import './firebase' // Import Firebase initialization
 
 const app = createApp(App)
 
 app.use(createPinia())
-
 
 app.mount('#app')
