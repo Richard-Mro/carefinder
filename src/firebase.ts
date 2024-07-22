1b57b29d2a736304e92ec4e2a1a3c79935516ede
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-22 23:54:00 +0000

    more -

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,10 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth, Auth } from 'firebase/auth'
-import firebaseConfig from '../dotenv.config' 
+import firebaseConfig from '../dotenv.config'
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore(app)
 const auth: Auth = getAuth(app)
 
