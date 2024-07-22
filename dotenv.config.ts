99c5e3ddd2af2241698d0c0463ffd2243f146057
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-22 21:50:23 +0000

    more

--- dotenv.config.ts
+++ dotenv.config.ts
@@ -1,20 +1,13 @@
-import dotenv from 'dotenv'
-import { resolve } from 'path'
-
-// Specify the path to your .env file
-const envFilePath = resolve(__dirname, '..', '.env')
-dotenv.config({ path: envFilePath })
-
 const firebaseConfig = {
-  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
-  databaseURL: process.env.VITE_APP_FIREBASE_DATABASE_URL,
-  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
-  googleMapsApiKey: process.env.VITE_APP_GOOGLE_MAPS_API_KEY
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY
 }
 
 export default firebaseConfig
--- src/firebase.mjs
+++ src/firebase.mjs
@@ -1,20 +1,7 @@
-// firebase.mjs
-
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  databaseURL: 'https://carefinder-70ff2-default-rtdb.asia-southeast1.firebasedatabase.app',
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY
-}
+import { firebaseConfig } from '../dotenv.config.mjs'
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore(app)
