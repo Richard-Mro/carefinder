971db57607623f1505e004398a947c26842f6d29
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-22 23:35:23 +0000

    more-

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,26 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onRequest } from 'firebase-functions/v1/https'
+import * as functions from 'firebase-functions'
+import * as logger from 'firebase-functions/logger'
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// Endpoint to expose Firebase configuration
+export const getFirebaseConfig = onRequest(
+  (request: functions.Request, response: functions.Response) => {
+    response.json({
+      apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+      authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+      databaseURL: process.env.VITE_APP_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VITE_FIREBASE_APP_ID,
+      measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+      googleMapsApiKey: process.env.VITE_APP_GOOGLE_MAPS_API_KEY
+    })
+  }
+)
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
- export const helloWorld = onRequest((request, response) => {
- logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+// Example function
+export const helloWorld = onRequest((request: functions.Request, response: functions.Response) => {
+  logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
