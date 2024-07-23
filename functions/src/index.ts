eeceb73dc79ed3a6fce67de556ede13e65529d63
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-23 22:26:38 +0000

    mores

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,16 @@
-import { onRequest } from 'firebase-functions/v1/https'
-import * as functions from 'firebase-functions'
-import * as logger from 'firebase-functions/logger'
+import { onRequest } from 'firebase-functions/v1/https';
+import * as functions from 'firebase-functions';
 
-export const getFirebaseConfig = onRequest(
-  (request: functions.Request, response: functions.Response) => {
-    response.json({
-      apiKey: functions.config().custom.firebase.api_key,
-      authDomain: functions.config().custom.firebase.auth_domain,
-      projectId: functions.config().custom.firebase.project_id,
-      databaseURL: functions.config().custom.firebase.database_url,
-      storageBucket: functions.config().custom.firebase.storage_bucket,
-      messagingSenderId: functions.config().custom.firebase.messaging_sender_id,
-      appId: functions.config().custom.firebase.app_id,
-      measurementId: functions.config().custom.firebase.measurement_id,
-      googleMapsApiKey: functions.config().custom.google.maps_api_key
-    })
-  }
-)
-
-export const helloWorld = onRequest((request: functions.Request, response: functions.Response) => {
-  logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+export const getFirebaseConfig = onRequest((request, response) => {
+  response.json({
+    apiKey: functions.config().custom.firebase.api_key,
+    authDomain: functions.config().custom.firebase.auth_domain,
+    projectId: functions.config().custom.firebase.project_id,
+    databaseURL: functions.config().custom.firebase.database_url,
+    storageBucket: functions.config().custom.firebase.storage_bucket,
+    messagingSenderId: functions.config().custom.firebase.messaging_sender_id,
+    appId: functions.config().custom.firebase.app_id,
+    measurementId: functions.config().custom.firebase.measurement_id,
+    googleMapsApiKey: functions.config().custom.google.maps_api_key,
+  });
+});
