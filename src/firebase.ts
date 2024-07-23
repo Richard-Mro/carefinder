64fe596afa67f95b94c308811855ea7d7230d055
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-23 00:48:18 +0000

    more update

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,20 @@ import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth, Auth } from 'firebase/auth'
 
-let app
-let db
-let auth: Auth
+let app: ReturnType<typeof initializeApp> | undefined
+let db: ReturnType<typeof getFirestore> | undefined
+let auth: Auth | undefined
 
 async function initializeFirebase() {
   const response = await fetch('/getFirebaseConfig')
   const firebaseConfig = await response.json()
 
-  app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
+  if (getApps().length === 0) {
+    app = initializeApp(firebaseConfig)
+  } else {
+    app = getApp()
+  }
+
   db = getFirestore(app)
   auth = getAuth(app)
 }
