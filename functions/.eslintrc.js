6ca6d529655a93147768a77b1ab516cce07f2524
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-25 14:34:30 +0100

    this as well for proper linting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,32 +2,30 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['./functions/tsconfig.json', './functions/tsconfig.dev.json'],
+    sourceType: 'module'
   },
-  ignorePatterns: [
-    "/lib/**/*", 
-    "/generated/**/*", 
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  ignorePatterns: ['/lib/**/*', '/generated/**/*'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
-};
+    quotes: ['error', 'double'],
+    'import/no-unresolved': 0,
+    indent: ['error', 2],
+    'object-curly-spacing': ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline']
+  }
+}
