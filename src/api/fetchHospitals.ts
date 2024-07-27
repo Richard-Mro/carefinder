db7fb4d70464b60ab700876e91ebe7c20c9790a8
Mololuwa Richard OLORUNSOLA <mololuwa.olorunsola@gmail.com>
2024-07-27 17:49:33 +0100

    upda

--- src/api/fetchHospitals.ts
+++ src/api/fetchHospitals.ts
@@ -1,11 +1,17 @@
-import axios from 'axios'
-
-const serverUrl = 'http://localhost:3000/api/hospitals'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
 
 export const fetchHospitals = async () => {
   try {
-    const response = await axios.get(serverUrl)
-    return response.data.results
+    if (!db) {
+      throw new Error('Firestore database is not initialized')
+    }
+
+    const hospitalsCollection = collection(db, 'hospitals')
+    const snapshot = await getDocs(hospitalsCollection)
+    const hospitalsList = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+
+    return hospitalsList
   } catch (error: any) {
     console.error('Error fetching hospitals:', error)
     throw new Error(error.toString())
